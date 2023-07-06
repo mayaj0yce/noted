311ba75e88d3a694d76d3bec96e1d4db3b116e93
mayaj0yce <m.j.thayne@gmail.com>
2023-07-06 16:10:04 -0600

    saving here after a study review

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,22 @@
 const router = require('express').Router();
 const path = require('path');
 
+
+router.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../db/db.json'))
+  
+})
+
+router.post('/api/notes', (req, res) => {
+    req.readFile(path)
+    res.sendFile(path.join(__dirname, '../db/db.json'))
+  //how does the heroku see this
+})
+module.exports = router 
+
 // enable user to modify db.json CRUD
 
 //google express CRUD routes
- router.post 
- router.get 
- /api/notes 
- 
+//  router.post 
+//  router.get 
+//  /api/notes 
